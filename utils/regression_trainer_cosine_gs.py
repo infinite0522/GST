a9d4825b54de4e8493efa8d32bd83e650cf8227d
miaos <infinite0522@163.com>
2025-11-19 17:20:04 +0800

    GST

---
+++ utils/regression_trainer_cosine_gs.py
@@ -0,0 +1,254 @@
+from utils.trainer import Trainer
+from utils.helper import Save_Handle, AverageMeter
+import os
+import sys
+import time
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.utils.data.dataloader import default_collate
+import logging
+import numpy as np
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from models.vgg_graph import vgg19_trans
+from models.vgg import vgg19
+from datasets.crowd_ucf_1gs import Crowd
+from losses.bay_loss import Bay_Loss
+from losses.post_prob_gs import Post_Prob_GS
+from math import ceil
+
+
+def train_collate(batch):
+    transposed_batch = list(zip(*batch))
+    images = torch.stack(transposed_batch[0], 0)
+    points = transposed_batch[1]  # the number of points is not fixed, keep it as a list of tensor
+    scales = transposed_batch[2]
+    rotations = transposed_batch[3]
+    targets = transposed_batch[4]
+    st_sizes = torch.FloatTensor(transposed_batch[5])
+    return images, points, scales, rotations, targets, st_sizes
+
+
+class RegTrainer(Trainer):
+    def setup(self):
+        """initial the datasets, model, loss and optimizer"""
+        args = self.args
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            self.device_count = torch.cuda.device_count()
+            # for code conciseness, we release the single gpu version
+            assert self.device_count == 1
+            logging.info('using {} gpus'.format(self.device_count))
+        else:
+            raise Exception("gpu is not available")
+
+        self.downsample_ratio = args.downsample_ratio
+        """ dataset preprocess:"""
+        self.datasets = {x: Crowd(os.path.join(args.data_dir, x),
+                                  args.crop_size,
+                                  args.downsample_ratio,
+                                  args.is_gray, x) for x in ['train', 'val']}
+        self.dataloaders = {x: DataLoader(self.datasets[x],
+                                          collate_fn=(train_collate
+                                                      if x == 'train' else default_collate),
+                                          batch_size=(args.batch_size
+                                                      if x == 'train' else 1),
+                                          shuffle=(True if x == 'train' else False),
+                                          num_workers=args.num_workers * self.device_count,
+                                          pin_memory=(True if x == 'train' else False))
+                            for x in ['train', 'val']}
+
+
+        assert args.model_name in ['vgg, vgg_trans'], f"{args.model_name} must be vgg or vgg_trans!"
+        if args.model_name=='vgg':
+            self.model = vgg19()
+        elif args.model_name=='vgg_trans':
+            self.model = vgg19_trans(args.topk, args.usenum)
+        self.model_name = args.model_name
+
+        self.model.to(self.device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+        self.start_epoch = 0
+        if args.resume:
+            suf = args.resume.rsplit('.', 1)[-1]
+            if suf == 'tar':
+                checkpoint = torch.load(args.resume, self.device)
+                print(args.resume)
+                self.model.load_state_dict(checkpoint['model_state_dict'])
+                self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+                self.start_epoch = checkpoint['epoch'] + 1
+            elif suf == 'pth':
+                self.model.load_state_dict(torch.load(args.resume, self.device))
+
+        self.post_prob = Post_Prob_GS(c_size=[args.crop_size, args.crop_size],
+                                      stride=args.downsample_ratio,
+                                      background_ratio=args.background_ratio,
+                                      use_background=args.use_background,
+                                      post_min=args.post_min,
+                                      post_max=args.post_max,
+                                      scale_ratio=args.scale_ratio,
+                                      device=self.device,
+                                      cut_off=args.cut_off
+                                      )
+        self.criterion = Bay_Loss(args.use_background, self.device)
+        self.save_list = Save_Handle(max_num=args.max_model_num)
+        self.best_mae = np.inf
+        self.best_mse = np.inf
+        self.save_all = args.save_all
+        self.best_test_mae = np.inf
+        self.best_test_mse = np.inf
+        self.best_count = 0
+        self.good_count = 0
+
+    def train(self):
+        """training process"""
+        args = self.args
+        for epoch in range(self.start_epoch, args.max_epoch):
+            logging.info('-'*5 + 'Epoch {}/{}'.format(epoch, args.max_epoch - 1) + '-'*5)
+            self.epoch = epoch
+            self.train_eopch()
+            if epoch % args.val_epoch == 0 and epoch >= args.val_start:
+                self.val_epoch()
+
+    def train_eopch(self):
+        epoch_loss = AverageMeter()
+        epoch_mae = AverageMeter()
+        epoch_mse = AverageMeter()
+        epoch_start = time.time()
+        self.model.train()  # Set model to training mode
+
+        # Iterate over data.
+        for step, (inputs, points, scales, rotations, targets, st_sizes) in enumerate(self.dataloaders['train']):
+            inputs = inputs.to(self.device)
+            st_sizes = st_sizes.to(self.device)
+            gd_count = np.array([len(p) for p in points], dtype=np.float32)
+            points = [p.to(self.device) for p in points]
+            targets = [t.to(self.device) for t in targets]
+
+            scales = [scale.to(self.device) for scale in scales]
+            rotations = [rotation.to(self.device) for rotation in rotations]
+
+            with torch.set_grad_enabled(True):
+                try:
+                    if self.model_name=='vgg':
+                        outputs = self.model(inputs)
+                        prob_list = self.post_prob(scales, rotations, points, st_sizes)
+                        loss = self.criterion(prob_list, targets, outputs)
+
+                    elif self.model_name=='vgg_trans':
+                        outputs, pe_list = self.model(inputs)
+                        prob_list = self.post_prob(scales, rotations, points, st_sizes)
+                        loss = self.criterion(prob_list, targets, outputs)
+                        for pe in pe_list:
+                            loss_pe = torch.var(pe, dim=-1)
+                            # loss_pe = torch.sum(loss_pe[loss_pe>0.1])
+                            loss_pe = torch.sum(loss_pe)
+                            loss += 0.1 * loss_pe
+                    loss.backward()
+
+                    assert not torch.isnan(loss), "Loss is NaN, terminating training and saving checkpoint."
+
+                except AssertionError as e:
+                    print(e)
+                    nan_save_path = os.path.join(self.save_dir, '{}_nan_ckpt.tar'.format(self.epoch))
+                    torch.save({
+                        'epoch': self.epoch,
+                        'optimizer_state_dict': self.optimizer.state_dict(),
+                        'model_state_dict':self.model.state_dict()
+                    }, nan_save_path)
+                    print(f"NAN Checkpoint saved to {nan_save_path}")
+
+                    exit(1)
+            if True:
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+                N = inputs.size(0)
+                pre_count = torch.sum(outputs.view(N, -1), dim=1).detach().cpu().numpy()
+                res = pre_count - gd_count
+                epoch_loss.update(loss.item(), N)
+                epoch_mse.update(np.mean(res * res), N)
+                epoch_mae.update(np.mean(abs(res)), N)
+
+        logging.info('Epoch {} Train, Loss: {:.2f}, MSE: {:.2f} MAE: {:.2f}, Cost {:.1f} sec'
+                     .format(self.epoch, epoch_loss.get_avg(), np.sqrt(epoch_mse.get_avg()), epoch_mae.get_avg(),
+                             time.time()-epoch_start))
+        model_state_dic = self.model.state_dict()
+        save_path = os.path.join(self.save_dir, '{}_ckpt.tar'.format(self.epoch))
+        torch.save({
+            'epoch': self.epoch,
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'model_state_dict': model_state_dic
+        }, save_path)
+        self.save_list.append(save_path)  # control the number of saved models
+
+    def val_epoch(self):
+        epoch_start = time.time()
+        self.model.eval()  # Set model to evaluate mode
+        epoch_res = []
+        # Iterate over data.
+        for inputs, count, name in self.dataloaders['val']:
+            inputs = inputs.to(self.device)
+            # inputs are images with different sizes
+            b, c, h, w = inputs.shape
+            h, w = int(h), int(w)
+            assert b == 1, 'the batch size should equal to 1 in validation mode'
+            input_list = []
+            c_size = 512
+            if h >= c_size or w >= c_size:
+                h_stride = int(ceil(1.0 * h / c_size))
+                w_stride = int(ceil(1.0 * w / c_size))
+                h_step = h // h_stride
+                w_step = w // w_stride
+                for i in range(h_stride):
+                    for j in range(w_stride):
+                        h_start = i * h_step
+                        if i != h_stride - 1:
+                            h_end = (i + 1) * h_step
+                        else:
+                            h_end = h
+                        w_start = j * w_step
+                        if j != w_stride - 1:
+                            w_end = (j + 1) * w_step
+                        else:
+                            w_end = w
+                        input_list.append(inputs[:, :, h_start:h_end, w_start:w_end])
+                with torch.set_grad_enabled(False):
+                    pre_count = 0.0
+                    for idx, input in enumerate(input_list):
+                        output = self.model(input)[0]
+                        pre_count += torch.sum(output)
+                res = count[0].item() - pre_count.item()
+                epoch_res.append(res)
+            else:
+                with torch.set_grad_enabled(False):
+                    outputs = self.model(inputs)[0]
+                    # save_results(inputs, outputs, self.vis_dir, '{}.jpg'.format(name[0]))
+                    res = count[0].item() - torch.sum(outputs).item()
+                    epoch_res.append(res)
+
+        epoch_res = np.array(epoch_res)
+        mse = np.sqrt(np.mean(np.square(epoch_res)))
+        mae = np.mean(np.abs(epoch_res))
+        logging.info('Epoch {} Val, MSE: {:.2f} MAE: {:.2f}, Cost {:.1f} sec'
+                     .format(self.epoch, mse, mae, time.time()-epoch_start))
+
+        model_state_dic = self.model.state_dict()
+        #logging.info("best mse {:.2f} mae {:.2f}".format(self.best_mse, self.best_mae))
+        if (2.0 * mse + mae) < (2.0 * self.best_mse + self.best_mae):
+            self.best_mse = mse
+            self.best_mae = mae
+            logging.info("save best mse {:.2f} mae {:.2f} model epoch {}".format(self.best_mse,
+                                                                                 self.best_mae,
+                                                                                 self.epoch))
+            if self.save_all:
+                torch.save(model_state_dic,
+                           os.path.join(self.save_dir, 'best_model_{}-epoch{}.pth'.format(self.best_count, self.epoch)))
+                self.best_count += 1
+            else:
+                torch.save(model_state_dic, os.path.join(self.save_dir, 'best_model.pth'))
+        elif mae < 80.0 and self.args.dataset == 'nwpu':
+            torch.save(model_state_dic,
+                       os.path.join(self.save_dir, 'good_model_{}-epoch{}.pth'.format(self.good_count, self.epoch)))
+            self.good_count += 1
